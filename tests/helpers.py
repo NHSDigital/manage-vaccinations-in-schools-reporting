e9630f602ae27f03ad6153d01a35f744d81b1103
Mike Thompson <mike@artifactual.co.uk>
2025-09-07 07:23:29 +0100

    Restructure mock user data and update to match Mavis

--- tests/helpers.py
+++ tests/helpers.py
@@ -6,30 +6,33 @@ def create_random_token():
 
 
 def mock_user_info():
-    jwt_data = {
-        "user": {
-            "id": 1,
-            "email": "nurse.joy@example.com",
-            "created_at": "2025-06-16T11:09:24.289+01:00",
-            "updated_at": "2025-07-04T10:11:36.100+01:00",
-            "provider": None,
-            "uid": None,
-            "given_name": "Nurse",
-            "family_name": "Joy",
-            "fallback_role": "nurse",
-        },
-        "cis2_info": {
-            "selected_org": {"code": "R1L", "name": "SAIS Organisation 1"},
-            "selected_role": {
-                "code": "S8000:G8000:R8001",
-                "workgroups": ["schoolagedimmunisations"],
+    return {
+        "jwt_data": {
+            "user": {
+                "id": 1,
+                "email": "nurse.joy@example.com",
+                "created_at": "2025-06-16T11:09:24.289+01:00",
+                "updated_at": "2025-07-04T10:11:36.100+01:00",
+                "provider": None,
+                "uid": None,
+                "given_name": "Nurse",
+                "family_name": "Joy",
+                "fallback_role": "nurse",
+                "show_in_suppliers": True,
+                "session_token": create_random_token(),
+                "reporting_api_session_token": create_random_token(),
+            },
+            "cis2_info": {
+                "activity_codes": "",
+                "has_other_roles": "",
+                "organisation_code": "R1L",
+                "organisation_name": "SAIS Organisation 1",
+                "role_code": "S8000:G8000:R8001",
+                "role_name": "",
+                "team_workgroup": "r1l",
+                "workgroups": ["r1l"],
             },
         },
-    }
-    jwt_data["user"]["session_token"] = create_random_token()
-    jwt_data["user"]["reporting_api_session_token"] = create_random_token()
-    return {
-        "jwt_data": jwt_data,
         "user_nav": {
             "items": [
                 {"text": "Test User", "icon": True},
