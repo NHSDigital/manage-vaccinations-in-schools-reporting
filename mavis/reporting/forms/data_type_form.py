b51b8e291894c5307955f91cf9c24ece023bedb0
Mike Thompson <mike@artifactual.co.uk>
2026-02-12 15:53:40 +0000

    Revert "Improve the hint text for reporting data downloads"

--- mavis/reporting/forms/data_type_form.py
+++ mavis/reporting/forms/data_type_form.py
@@ -16,13 +16,11 @@ class DataTypeForm(FlaskForm):
         validators=[InputRequired(message="Select which data you want to download")],
         choices=[
             (CHILD_RECORDS, "Child-level vaccination data"),
-            (AGGREGATE_DATA, "Aggregate vaccination and consent data"),
+            (AGGREGATE_DATA, "Aggregate vaccination data"),
         ],
     )
 
     hints = {
-        CHILD_RECORDS: "Full vaccination details for individual children. Only \
-            includes vaccinations given by your team.",
-        AGGREGATE_DATA: "Total figures for all children in your cohort. Includes \
-            vaccinations given by any provider.",
+        CHILD_RECORDS: "Vaccinations given by your team",
+        AGGREGATE_DATA: "Vaccination status for all children in your cohort",
     }
